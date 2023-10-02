a6e576bb2a4325c1e494f38b064b048b188087db
Maciej Pilarski <maciekpilar12@gmail.com>
2023-10-02 11:00:36 +0200

    Footer link to YT

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -44,7 +44,14 @@ const Footer = () => (
 
     <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]">
       <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">
-        Copyright Ⓒ 2022 HooBank. All Rights Reserved.
+        Copyright Ⓒ 2023 HooBank | All Rights Reserved | Page created to
+        practice with{" "}
+        <a
+          href="https://www.youtube.com/@javascriptmastery"
+          className="hover:underline"
+        >
+          JS Mastery YouTube
+        </a>
       </p>
 
       <div className="flex flex-row md:mt-0 mt-6">
